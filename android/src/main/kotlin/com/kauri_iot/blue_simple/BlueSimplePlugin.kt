084e1356447aa3857618bbea7b8cf142eb425dd3
Aleksei Vasilev <vasiliev.an@edu.spbstu.ru>
2021-11-25 12:16:53 +0300

    // some changes.

--- android/src/main/kotlin/com/kauri_iot/blue_simple/BlueSimplePlugin.kt
+++ android/src/main/kotlin/com/kauri_iot/blue_simple/BlueSimplePlugin.kt
@@ -7,6 +7,10 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import java.util.*
+
 
 /** BlueSimplePlugin */
 
